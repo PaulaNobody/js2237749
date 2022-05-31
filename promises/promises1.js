716e848cdf5d8c30c0512f91e19c7782be7b47db
PaulaNobody <1070331217cc@gmail.com>
2022-05-31 00:15:10 -0500

    promise:Ejemplo del profe

--- promises/promises1.js
+++ promises/promises1.js
@@ -88,3 +88,35 @@ obtenerUsuario(1)
 .catch(error=> {
     console.log(error);
 });
+
+function aguaHervida(control){
+    return new Promise((resolve,reject)=>{            
+        if (control) {
+            setTimeout(() => {
+                console.log(`Proceso de hervimiento de agua terminado`)        
+                    resolve()
+                },3000)        
+        } else {
+            reject(error);            
+        }
+        
+    })   
+}
+function agregarIngredientes(){
+    console.log(`Todos los ingredientes listos`)    
+}
+function picarCarne(){
+    console.log('Lista la carne')
+}
+function pelarPapa(){
+    console.log('Lista la papa')
+}
+ function lavarVerduras(){
+    console.log('Listas las verduras')
+}
+ aguaHervida(false)
+ .then(agregarIngredientes)
+ .catch(()=>console.log('No hay GAS'))
+ pelarPapa()
+ lavarVerduras()
+ picarCarne()
